9ef48405520890f7711d3cecf433800ad39a8834
tianshapojun <383033930@qq.com>
2023-04-20 10:09:24 +0800

    Update load_dataset.py

--- lib/load_dataset.py
+++ lib/load_dataset.py
@@ -4,12 +4,13 @@ import numpy as np
 def load_st_dataset(dataset):
     #output B, N, D
     if dataset == 'PEMSD4':
-        #data_path = os.path.join('../data/PeMSD4/pems04.npz')
-        #data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
-        data = np.load('/content/AGCRN/data/Macro/samples.npy')[:,:,0]
+        data_path = os.path.join('../data/PeMSD4/pems04.npz')
+        data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
     elif dataset == 'PEMSD8':
         data_path = os.path.join('../data/PeMSD8/pems08.npz')
         data = np.load(data_path)['data'][:, :, 0]  #onley the first dimension, traffic flow data
+    elif dataset =='SAMPLES':
+        data = np.load('../data/Macro/samples.npy')[:,:,0]
     else:
         raise ValueError
     if len(data.shape) == 2:
